24bdc5ce1ce99d897ddc7a4bdf29d09d327c9968
fabio mota alg3 <fabioferreiradamota17@hotmail.com>
2018-10-15 15:52:49 -0400

    teste permissão

--- conexão.cs
+++ conexão.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.Data;
-//teste braçofabio teste carlos
+//teste braçofabio teste carlos teste permissao
 namespace TrabalhoAlg3
 {
     class Conexão
