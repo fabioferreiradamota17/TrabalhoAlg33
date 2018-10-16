00d7999110ef61d2898c2ea7f7b7d6ad3d355cc2
Carlos Ramon <ramon.ethan@gmail.com>
2018-10-16 07:21:58 -0400

    Alteração do meu Banco local

--- TrabalhoAlg3/conexão.cs
+++ TrabalhoAlg3/conexão.cs
@@ -7,13 +7,15 @@ using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using System.Data;
 
-namespace TrabalhoAlg3
+namespace TrabalhoAlg3//carlos
 {
     class Conexão
     {
         SqlConnection sqlcon = null;
-        private string StrCon = "Data Source=DESKTOP-BDHQ8NC\\FABIO;" +"Initial Catalog=BANCOFABIO;" +"User id=sa;" +"Password=612914;";
         
+        //Banco Fabio -> private string StrCon = "Data Source=DESKTOP-BDHQ8NC\\FABIO;" + "Initial Catalog=BANCOFABIO;" + "User id=sa;" + "Password=612914;";
+        private string StrCon = "Data Source=DESKTOP-6N7BJMH;" + "Initial Catalog=alg3;" + "User id=sa;" + "Password=vssql;";
+
         string StrSql = string.Empty;
 
         public void SalvarCadastro(string Nome,string Cpf, string Rg,string Telefone)
